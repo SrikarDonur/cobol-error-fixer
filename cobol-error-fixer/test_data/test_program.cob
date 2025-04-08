f1f642ec54efa8f666fba2dec61d3aaa710558a9
SrikarDonur <srikardonur@gmail.com>
2025-04-09 00:42:49 +0530

    Fix: Updated COBOL function

--- cobol-error-fixer/test_data/test_program.cob
+++ cobol-error-fixer/test_data/test_program.cob
@@ -1,12 +1,20 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. TESTPROG.
-
-       DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 TOTAL         PIC 9(5) VALUE ZERO.
+       01 EOF-REACHED    PIC X(3) VALUE 'NO'.
 
        PROCEDURE DIVISION.
-           DISPLAY "Starting program..."
-           suuuuuuuiii
-           DISPLAY "Total is: ", TOTAL
+       MAIN-PARA.
+           OPEN INPUT TRANSACTION-FILE
+           PERFORM UNTIL EOF-REACHED = 'YES'
+               READ TRANSACTION-FILE
+                   AT END
+                       DISPLAY "End of File."
+                       MOVE 'YES' TO EOF-REACHED
+                   NOT AT END
+                       DISPLAY "Processing Record."
+                       MOVE ACCOUNT-NUMBER TO WS-ACCOUNT-NUMBER
+                       MOVE TRANSACTION-AMT TO WS-TRANSACTION-AMT
+                       DISPLAY "Account: " WS-ACCOUNT-NUMBER " Amount: " WS-TRANSACTION-AMT
+               END-READ
+           END-PERFORM
+           CLOSE TRANSACTION-FILE
            STOP RUN.
