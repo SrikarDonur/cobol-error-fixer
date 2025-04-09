824bd61e9610d6078d0c940d2b689e9c061affcc
SrikarDonur <srikardonur@gmail.com>
2025-04-09 16:06:40 +0530

    Fix: Updated COBOL function

--- cobol-error-fixer/test_data/test_program.cob
+++ cobol-error-fixer/test_data/test_program.cob
@@ -1,1 +1,16 @@
-suiiiii
+       PROCEDURE DIVISION.
+           OPEN INPUT TRANSACTION-FILE.
+           
+           READ TRANSACTION-FILE INTO TRANS-RECORD
+               AT END
+                   DISPLAY "End of File."
+               NOT AT END
+                   DISPLAY "Processing Record."
+                   MOVE ACCOUNT-NUMBER TO WS-ACCOUNT-NUMBER
+                   MOVE TRANSACTION-AMT TO WS-TRANSACTION-AMT
+                   DISPLAY "Account: " WS-ACCOUNT-NUMBER " Amount: " WS-TRANSACTION-AMT
+           END-READ.
+           
+           CLOSE TRANSACTION-FILE.
+           
+           STOP RUN.suuuuuiiiiiiiiii
