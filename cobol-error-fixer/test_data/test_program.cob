23e36e7b4b9b53c5ffb958c4ea971a2db1d85b41
SrikarDonur <srikardonur@gmail.com>
2025-04-09 16:10:57 +0530

    Fix: Updated COBOL function

--- cobol-error-fixer/test_data/test_program.cob
+++ cobol-error-fixer/test_data/test_program.cob
@@ -1,16 +1,20 @@
+       WORKING-STORAGE SECTION.
+       01 EOF-REACHED    PIC X(3) VALUE 'NO'.
+
        PROCEDURE DIVISION.
-           OPEN INPUT TRANSACTION-FILE.
-           
-           READ TRANSACTION-FILE INTO TRANS-RECORD
-               AT END
-                   DISPLAY "End of File."
-               NOT AT END
-                   DISPLAY "Processing Record."
-                   MOVE ACCOUNT-NUMBER TO WS-ACCOUNT-NUMBER
-                   MOVE TRANSACTION-AMT TO WS-TRANSACTION-AMT
-                   DISPLAY "Account: " WS-ACCOUNT-NUMBER " Amount: " WS-TRANSACTION-AMT
-           END-READ.
-           
-           CLOSE TRANSACTION-FILE.
-           
-           STOP RUN.suuuuuiiiiiiiiii
+       MAIN-PARA.
+           OPEN INPUT TRANSACTION-FILE
+           PERFORM UNTIL EOF-REACHED = 'YES'
+               READ TRANSACTION-FILE
+                   AT END
+                       DISPLAY "End of File."
+                       MOVE 'YES' TO EOF-REACHED
+                   NOT AT END
+                       DISPLAY "Processing Record."
+                       MOVE ACCOUNT-NUMBER TO WS-ACCOUNT-NUMBER
+                       MOVE TRANSACTION-AMT TO WS-TRANSACTION-AMT
+                       DISPLAY "Account: " WS-ACCOUNT-NUMBER " Amount: " WS-TRANSACTION-AMT
+               END-READ
+           END-PERFORM
+           CLOSE TRANSACTION-FILE
+           STOP RUN. fwehbhaeaaaaaaaaaaaaeffff
